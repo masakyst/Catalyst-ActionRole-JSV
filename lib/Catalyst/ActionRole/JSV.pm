77eca2066d78dfa053cae7ab8bc509675c6b8516
Masaaki Saito <masakyst.mobile@gmail.com>
2017-09-06 11:14:59 +0900

    request validation

--- lib/Catalyst/ActionRole/JSV.pm
+++ lib/Catalyst/ActionRole/JSV.pm
@@ -11,6 +11,7 @@ our $VERSION = '0.01';
 
 our $JSV;
 
+
 after BUILD => sub {
     my $class = shift;
     my ($args) = @_;
@@ -24,24 +25,46 @@ around execute => sub {
     my $self = shift;
     my ($controller, $c) = @_;
 
-    my $params = $c->req->body_data;
-    my $request_schema = JSON::MaybeXS::decode_json(Path::Class::file($c->config->{home}, $self->attributes->{Request}->[0])->slurp);
+    my $params = $c->req->parameters;
 
+    my $load_schema_json = Path::Class::file($c->config->{home}, $self->attributes->{JSONSchema}->[0]);
+    $c->log->debug("load file json schema file: ".$load_schema_json->stringify);
+
+    my $request_schema = JSON::MaybeXS::decode_json(Path::Class::file($c->config->{home}, $self->attributes->{JSONSchema}->[0])->slurp);
+
+    # find url capture args
+    if (scalar @{ $c->req->arguments}) {
+        # json property "captureargs" : "number"
+        for my $key (keys %{ $request_schema->{properties} }) {
+            if ($request_schema->{properties}->{$key}->{captureargs}) {
+                my $captureval = $c->req->arguments->[$request_schema->{properties}->{$key}->{captureargs} - 1];
+                if (defined $captureval) {
+                    $params->{$key} = $captureval;
+                }
+            }
+        }
+    }
+ 
     my $request_result = $JSV->validate($request_schema, $params);
 
     if ($request_result->get_error) {
+        $c->log->debug("json schema validation failed: ".$request_result->errors->[0]->{message});
+        # todo: return response
         $c->response->status(400);
         $c->stash->{json} = {message => sprintf("%s: %s", $request_result->errors->[0]->{pointer}, $request_result->errors->[0]->{message})};
         return;
     }
+    $c->log->debug("json schema validation success.");
 
     my $orig_response = $self->$orig(@_);
+
+    # todo: validate response schema ..
 
     return $orig_response;
 };
 
-
 1;
+
 __END__
 
 =encoding utf-8
